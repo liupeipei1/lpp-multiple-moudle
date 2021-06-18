a5bd7a8788837f20996d80f35a213cab563d1951
刘佩佩 <m_18664333740@163.com>
2021-06-18 17:56:36 +0800

    多模块代码支持spring boot

--- mybatis-learning/src/main/java/com/example/MybatisApplication.java
+++ mybatis-learning/src/main/java/com/example/MybatisApplication.java
@@ -4,9 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-@MapperScan("com.example.Dao") //扫描mapper 接口
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })@MapperScan("com.example.Dao") //扫描mapper 接口
 public class MybatisApplication {
 
     public static void main(String[] args) {
