9c6c4bffb2b4f214b348aad831bc2f35e0e7088f
刘佩佩 <m_18664333740@163.com>
2021-06-25 16:38:08 +0800

    添加druid 界面监控

--- mybatis-learning/src/main/java/com/example/config/ClusterDataSourceConfig.java
+++ mybatis-learning/src/main/java/com/example/config/ClusterDataSourceConfig.java
@@ -22,7 +22,7 @@ import javax.sql.DataSource;
 @MapperScan(basePackages = ClusterDataSourceConfig.PACKAGE, sqlSessionFactoryRef = "clusterSqlSessionFactory")
 public class ClusterDataSourceConfig {
     static final String MAPPER_LOCATION = "classpath:mapper/cluster/*.xml";
-    static final String PACKAGE = "com.example.Dao.second";
+    static final String PACKAGE = "com.example.Dao.second"; //mapper文件夹
 
     @Value("${cluster.datasource.url}")
     private String url;
--- mybatis-learning/src/main/java/com/example/config/MasterDataSourceConfig.java
+++ mybatis-learning/src/main/java/com/example/config/MasterDataSourceConfig.java
@@ -2,12 +2,16 @@ package com.example.config;
 
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -15,6 +19,10 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 注入数据源
  */
@@ -77,5 +85,39 @@ public class MasterDataSourceConfig {
         sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver()
                 .getResources(MasterDataSourceConfig.MAPPER_LOCATION));  //设置XML地址
         return sessionFactory.getObject();
+    }
+
+    /**
+     * 配置Druid监控
+     * web：http://localhost:8082/druid
+     * @return ServletRegistrationBean
+     * 原文链接：https://blog.csdn.net/qq_27634797/article/details/100560118
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+        Map<String, String> map = new HashMap<>();
+        map.put(StatViewServlet.PARAM_NAME_USERNAME, "lpp");  //访问的用户名密码
+        map.put(StatViewServlet.PARAM_NAME_PASSWORD, "123456");
+        //允许访问的ip，默认是所有ip
+        map.put(StatViewServlet.PARAM_NAME_ALLOW, "");
+        //禁止访问的ip
+        // map.put(StatViewServlet.PARAM_NAME_DENY, "192.168.1.1");
+        bean.setInitParameters(map);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean<WebStatFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new WebStatFilter());
+        Map<String, String> map = new HashMap<>();
+        //移除这些监听
+        map.put(WebStatFilter.PARAM_NAME_EXCLUSIONS, "*.js,*.css,/druid/*,*.gif,*.jpg,*.png");
+        bean.setInitParameters(map);
+        //拦截所有请求，全部都要走druid监听
+        bean.setUrlPatterns(Collections.singletonList("/*"));
+        return bean;
+        //原文链接：https://blog.csdn.net/qq_27634797/article/details/100560118
     }
 }
