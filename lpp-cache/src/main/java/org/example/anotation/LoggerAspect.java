dc73fd068f0f71296349fc08f9a5d700ad3a46cb
liupeipei <m_1866433740@163.com>
2023-12-03 18:20:25 +0800

    add jar in mybatis

--- lpp-cache/src/main/java/org/example/anotation/LoggerAspect.java
+++ lpp-cache/src/main/java/org/example/anotation/LoggerAspect.java
@@ -1,6 +1,5 @@
 package org.example.anotation;
 
-import lombok.extern.apachecommons.CommonsLog;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -27,11 +26,11 @@ public class LoggerAspect {
         stopwatch.start();
         String method = joinPoint.getSignature().getName();//得到方法
         String className = joinPoint.getTarget().getClass().getSimpleName();
-        log.info("========" + className + "." + method + "====begin");
+      //  log.info("========" + className + "." + method + "====begin");
         Object obj = joinPoint.proceed();
         stopwatch.stop();
         double second = stopwatch.getTotalTimeSeconds();
-        log.info("========" + className + "." + method + "====end  耗时" + second);
+       // log.info("========" + className + "." + method + "====end  耗时" + second);
         return obj;
     }
 }
