5330c40bc7d0b27a3931640d7f965526bdb91520
liupeipei <m_1866433740@163.com>
2024-04-21 13:51:05 +0800

    add the server in eureka server to listen other server status.

--- spring-eureka-server/src/main/java/com/example/springeuraka/SpringEurekaApplication.java
+++ spring-eureka-server/src/main/java/com/example/springeuraka/SpringEurekaApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = {GsonAutoConfiguration.class})
+@EnableEurekaServer
 public class SpringEurekaApplication {
 
     public static void main(String[] args) {
