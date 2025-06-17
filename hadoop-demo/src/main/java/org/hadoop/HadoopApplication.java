4d41332abfed4548a44449aff0674126d5fee796
liu,peipei <m_18664333740@163.com>
2025-06-17 12:55:10 +0800

    set demo for hadoop

--- hadoop-demo/src/main/java/org/hadoop/HadoopApplication.java
+++ hadoop-demo/src/main/java/org/hadoop/HadoopApplication.java
@@ -4,6 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HadoopApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CacheMain.class, args);
+        SpringApplication.run(HadoopApplication.class, args);
     }
 }
