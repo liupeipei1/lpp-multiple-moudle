b51190bf74576c3cbe771ec7254737b0add0d482
liu,peipei <m_18664333740@163.com>
2025-11-17 19:38:40 +0800

    leetcode hot 100  - 矩阵归零

--- common/src/main/java/com/example/common/基础/leetcode/矩阵置零.java
+++ common/src/main/java/com/example/common/基础/leetcode/矩阵置零.java
@@ -1,5 +1,11 @@
 package com.example.common.基础.leetcode;
 //给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+/*
+ 输入：matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+    输出：[[0,0,0,0],
+          [0,4,5,0],
+          [0,3,1,0]]
+ */
 public class 矩阵置零 {
     public static void main(String[] args) {
         int[][] matrix = {
