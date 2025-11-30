ded51859d9e8d6b9d849a21dda9e93aed3a5833a
liu,peipei <m_18664333740@163.com>
2025-11-30 21:00:23 +0800

    HR: 二叉树查到深度

--- common/src/main/java/com/example/common/基础/leetcode/TreeNode.java
+++ common/src/main/java/com/example/common/基础/leetcode/TreeNode.java
@@ -2,10 +2,10 @@ package com.example.common.基础.leetcode;
 
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
 
-    TreeNode(int x) {
+    public TreeNode(int x) {
         val = x;
     }
     TreeNode(int val, TreeNode left, TreeNode right) {
