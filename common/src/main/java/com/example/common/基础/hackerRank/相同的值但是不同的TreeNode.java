ed0bc3e6d2950e4e0f53d95142bb8f2a3ab90f1f
liu,peipei <m_18664333740@163.com>
2025-12-02 20:37:54 +0800

    LC 儿茶搜索树找到第k小的元素

---
+++ common/src/main/java/com/example/common/基础/hackerRank/相同的值但是不同的TreeNode.java
@@ -0,0 +1,18 @@
+package com.example.common.基础.hackerRank;
+/*
+ Given two binary search trees root1 and root2, return true if they contain
+ the same multiset of values but have different structures, otherwise return false.
+
+ root1 = [4, 2, 5, 1, 3, 100001, 100001]
+root2 = [3, 1, 5, 100001, 2, 4, 100001]
+
+output： true
+这里的100001是null
+这里可以节点的对应数组的位置是 按照根节点 左右
+  root 1
+        4
+     2     5
+   1   3  null,null
+ */
+public class 相同的值但是不同的TreeNode {
+}
--- common/src/main/java/com/example/common/基础/leetcode/二叉树的中序遍历.java
+++ common/src/main/java/com/example/common/基础/leetcode/二叉树的中序遍历.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 /*
  给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
-
 遍历是从左 中 右
     1
       2
